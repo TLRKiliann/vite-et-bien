6143e225dc604ad8395f648439ee2a51db73b19c
TLRKiliann <philogenie@protonmail.com>
2022-10-25 21:54:48 +0200

    routing with react-router-dom ok & server to getAll ok

--- my-vite/backend/server.js
+++ my-vite/backend/server.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 const PORT = 5000;
 
-console.log("Ok for cl")
+const date = new Date();
+
+const notes = [
+	{
+		'id': 1,
+		'name': 'Doug',
+		'age': 44
+	},
+	{
+		'id': 2,
+		'name': 'Jess',
+		'age': 25
+	},
+	{
+		'id': 3,
+		'name': 'Celestine',
+		'age': 38
+	}
+];
+
+app.get('/notes', (request, response) => {
+	response.status(200).json(notes)
+});
+
+app.get('/notes/:id', (request, response) => {
+	const id = Number(request.params.id);
+	console.log(id)
+	const note = notes.find(note => note.id === id); 
+	response.status(200).json(note)
+});
+
+app.get('/info', (request, response) => {
+	const msg = `This is a page information : ${notes.length} ${date}`;
+	response.status(200).send(msg)
+});
 
 app.use(express.json());
 
